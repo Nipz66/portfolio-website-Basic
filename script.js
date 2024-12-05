e3626a359d886ede65668a15d62f52013507a16e
Nipun Anupama <anupamaalahakoon66@gmail.com>
2024-12-06 00:43:09 +0800

    Certificate Section Add

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ document.addEventListener("DOMContentLoaded", function () {
         observer.observe(section);
     });
 });
+//certificate
+// Show the pop-up when hovering over any certificate item
+const certificateItems = document.querySelectorAll('.certificate-item');
+
+certificateItems.forEach(item => {
+    item.addEventListener('mouseenter', function () {
+        const popup = this.querySelector('.certificate-popup');
+        popup.style.display = 'flex'; // Show the pop-up
+    });
+
+    item.addEventListener('mouseleave', function () {
+        const popup = this.querySelector('.certificate-popup');
+        popup.style.display = 'none'; // Hide the pop-up
+    });
+});
+
+// Close the pop-up when clicked anywhere on the pop-up background
+const certificatePopups = document.querySelectorAll('.certificate-popup');
+
+certificatePopups.forEach(popup => {
+    popup.addEventListener('click', function () {
+        this.style.display = 'none'; // Close pop-up
+    });
+});
